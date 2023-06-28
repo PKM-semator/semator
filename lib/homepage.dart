2c215f66387a09f12fb8e7193b583bd456f36a9c
arkanwicaksono1602@gmail.com <arkanwicaksono1602@gmail.com>
2023-06-28 19:40:26 +0700

    pengerjaan intro splash

---
+++ lib/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('Homepages'),
+      ),
+    );
+  }
+}
